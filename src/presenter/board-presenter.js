7f97a194ea7401286eacbc332ebd5b90510c6d9f
FortySixAnd2 <jmaxj91j@gmail.com>
2024-12-18 17:53:16 +0300

    feat: add event-create-view.

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -2,6 +2,7 @@ import { render } from '../render.js';
 import BoardView from '../view/board-view.js';
 import SortView from '../view/sort-view.js';
 import EventListView from '../view/event-list-view.js';
+import EventCreateView from '../view/event-create-view.js';
 import EventEditView from '../view/event-edit-view.js';
 
 
@@ -18,6 +19,7 @@ export default class BoardPresenter {
     render(this.boardComponent, this.boardContainer);
     render(new SortView(), this.boardComponent.getElement());
     render(this.eventListComponent, this.boardComponent.getElement());
+    render(new EventCreateView(), this.eventListComponent.getElement());
     render(new EventEditView(), this.eventListComponent.getElement());
   }
 }
