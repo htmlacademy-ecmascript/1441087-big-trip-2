15a5b2d70f937b6e73f7232f1ad14929c34b4077
FortySixAnd2 <jmaxj91j@gmail.com>
2024-12-18 17:42:01 +0300

    feat: add event-edit-view.

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -1,17 +1,23 @@
 import { render } from '../render.js';
 import BoardView from '../view/board-view.js';
 import SortView from '../view/sort-view.js';
+import EventListView from '../view/event-list-view.js';
+import EventEditView from '../view/event-edit-view.js';
 
 
 export default class BoardPresenter {
   boardComponent = new BoardView();
+  eventListComponent = new EventListView();
 
   constructor({boardContainer}) {
     this.boardContainer = boardContainer;
   }
 
+
   init () {
     render(this.boardComponent, this.boardContainer);
     render(new SortView(), this.boardComponent.getElement());
+    render(this.eventListComponent, this.boardComponent.getElement());
+    render(new EventEditView(), this.eventListComponent.getElement());
   }
 }
