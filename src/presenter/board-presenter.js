9340e14a1431035d7cf5b7f650351cb8a6c182f5
FortySixAnd2 <jmaxj91j@gmail.com>
2024-12-18 18:31:29 +0300

    feat: add event-item-view and event-view.

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -4,6 +4,8 @@ import SortView from '../view/sort-view.js';
 import EventListView from '../view/event-list-view.js';
 import EventCreateView from '../view/event-create-view.js';
 import EventEditView from '../view/event-edit-view.js';
+import EventItemView from '../view/event-item-view.js';
+import EventView from '../view/event-view.js';
 
 
 export default class BoardPresenter {
@@ -19,7 +21,20 @@ export default class BoardPresenter {
     render(this.boardComponent, this.boardContainer);
     render(new SortView(), this.boardComponent.getElement());
     render(this.eventListComponent, this.boardComponent.getElement());
-    render(new EventCreateView(), this.eventListComponent.getElement());
-    render(new EventEditView(), this.eventListComponent.getElement());
+
+    for (let i = 0; i < 3; i++) {
+      const eventItem = new EventItemView();
+      render(new EventView(), eventItem.getElement());
+
+      if (i === 0) {
+        render(new EventCreateView(), eventItem.getElement());
+      }
+
+      if (i === 1) {
+        render(new EventEditView(), eventItem.getElement());
+      }
+
+      render(eventItem, this.eventListComponent.getElement());
+    }
   }
 }
