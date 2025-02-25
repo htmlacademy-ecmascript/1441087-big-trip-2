42e45cbcd17c51da0af5c6ff5cd0fd962c0ac770
FortySixAnd2 <jmaxj91j@gmail.com>
2025-02-25 14:51:12 +0300

    refactor: change utils functions parameters naming.

--- src/utils/common-utils.js
+++ src/utils/common-utils.js
@@ -1,9 +1,9 @@
-function getCapitalizedString(string) {
-  return string[0].toUpperCase() + string.slice(1);
+function getCapitalizedString(data) {
+  return data[0].toUpperCase() + data.slice(1);
 }
 
-function getHtmlSafeString(string) {
-  return string.replace(/\s+/g, '-').toLowerCase();
+function getHtmlSafeString(data) {
+  return data.replace(/\s+/g, '-').toLowerCase();
 }
 
 function isEscapeKey (evt) {
